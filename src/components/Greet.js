d7666d4e50490ec3803f461c372c41e28cb7c7f7
Nipun Tewari <nipuntewari11@gmail.com>
2021-07-10 19:52:14 +0530

    destructuring in state and props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,14 +3,21 @@ import React from "react";
 // props are similar to arguments that can be passed to the function (or class) and changes the state accordingly
 // note, how only {} is required to add a variable and not ${}
 
-const Greet = (props) => {
-  //   console.log(props);
+// destructuring is done using curly braces in two ways
+// const Greet = (props) => {
+
+//! first way - while calling the function
+// !const Greet = ({name, heroName, children}) => {
+
+//? second way = inside the function body
+  const Greet = (props) => {
+const {name, heroName, children} = props
   return (
     <div>
       <h1>
-        Hello {props.name} aka {props.heroName}
+        Hello {name} aka {heroName}
       </h1>
-      {props.children}
+      {children}
     </div>
   );
 };
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 class Welcome extends Component {
   render() {
+    // destructuring props in classes
+    //! const {name, heroName} = this.props
+
+    // destructuring states
+    //? const {sate1, state2} = this.state
     return (
       <h1>
         Welcome {this.props.name} aka {this.props.heroName}
